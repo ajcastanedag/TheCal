0dd6106e47d4fed2d3c828599246d37658737770
ajcastanedag <antonio.castanedag@gmail.com>
2023-10-24 15:00:35 +0200

    base

--- App/Main.R
+++ App/Main.R
@@ -29,17 +29,17 @@ ui <-shinydashboard::dashboardPage(
     )
   ),
   dashboardBody(
-    tags$head(
-      # Include our custom CSS
-      includeCSS(Style)
-      #tags$link(rel = "stylesheet", type = "text/css", href = "/www/2_Style/UASstyle.css")
-    ),
     tabItems(
+      # In the "Instructions" tab content
       tabItem(tabName = "instructions",
-              h2("Instructions Tab Content"),
-              p("This is where you can provide instructions or information for users."),
-              p("You can use this tab to explain how to use the app, provide guidance, or any other relevant details."),
-              # Add content for this tab here
+
+              # Add a radio button group to select the operating system
+              radioButtons(inputId = "os_selector", label = "Select Operating System",
+                           choices = c("Linux", "Windows"), inline = TRUE),
+              
+              # Add a button to get the SDK
+              actionButton("get_sdk_button", "GET SDK"),
+              
       ),
       tabItem(tabName = "folder_structure",
               h2("Create Folder Structure Tab Content"),
@@ -61,12 +61,18 @@ ui <-shinydashboard::dashboardPage(
 # Define the server logic
 server <- function(input, output, session) {
   # Add server logic for each tab here if needed
+  
+  # Define the observeEvent to call getSDK when the button is clicked
+  observeEvent(input$get_sdk_button, {
+    getSDK("Lin", paste0(Root,"/www/1_SDK"))
+  })
+  
+  
 }
 
 # Create Shiny app
 shinyApp(ui, server)
 
-getSDK("Lin", paste0(Root,"/www/1_SDK"))
 
 
 
